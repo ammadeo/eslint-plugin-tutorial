6fd1aebcfd679cc6419590e9fddb64a09fc4a31e
ammadeo <amadeuszchomiak@gmail.com>
2021-08-11 17:55:36 +0200

    remove no-literal rule

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import noLiteral from "./rules/no-literal";
 import isPrefixForBoolean from "./rules/is-prefix-for-boolean";
 
 export = {
   rules: {
-    "no-literal": noLiteral,
     "is-prefix-for-boolean": isPrefixForBoolean,
   },
 };
